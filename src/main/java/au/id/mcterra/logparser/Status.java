3527181223cdc0ff5a237139e43b4fa9da90c5ff
Terra <terra@mcterra.id.au>
2021-10-19 12:58:04 +1100

    lol stupid moment
    Signed-off-by: Terra <terra@mcterra.id.au>

--- src/main/java/au/id/mcterra/logparser/Status.java
+++ src/main/java/au/id/mcterra/logparser/Status.java
@@ -4,12 +4,12 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class Status {
-	public final int roundId;
+	public final long roundId;
 
 	public Status(String text) throws Exception {
 		JSONParser parser = new JSONParser();
 		Object result = parser.parse(text);
 		JSONObject json = (JSONObject)result;
-		roundId = (int)json.get("round_id");
+		roundId = (long)json.get("round_id");
 	}
 }
